760640c80beb50e4428d0f7c4ccb086f4e997832
MaximNatsun <maxim.natsun93@gmail.com>
2021-06-27 22:00:04 +0300

    Сдача 4-го ДЗ (1 часть)

---
+++ 4_1_Homework.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] myArray = new int[20];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = (int) (Math.random() * 20 - 10);
+        }
+
+        // определение максимального отрицательного и минимального положительного элементов массива
+        int max = -10;
+        int min = 10;
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] < 0 && myArray[i] > max) {
+                max = myArray[i];
+            }
+            if (myArray[i] > 0 && myArray[i] < min) {
+                min = myArray[i];
+            }
+        }
+        System.out.printf("Максимальный отрицательный элемент: %d. Минимально положительный элемент: %d", max, min);
+
+        // меняем местами через третью переменную
+        int temp;
+        temp = min;
+        min = max;
+        max = temp;
+        System.out.printf("\nПоменяли местами: \nМаксимальный отрицательный элемент: %d. Минимально положительный элемент: %d", max, min);
+    }
+}
